c3d7d01ad3c47abe8cd0a9b9299b2ec138819816
Kyle West <wes12009@byui.edu>
2017-04-20 17:40:20 -0600

    Created Hello World Page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
 <?php
-
-  phpinfo();
+   
+   // the classic message
+   $hello = "Hello World!";
+   
+   // Print out each letter of the message with each letter increasing in size.
+   for ($i = 0; $i < strlen($hello); $i++) {
+      print "<span style = 'font-size:".(($i+5)*10)."pt;'>".$hello[$i]."<span>";
+   }
 
 ?>
