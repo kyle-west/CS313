0a9f7c28d5c81bf2bb8948eab35f496d7530b0b2
Kyle West <wes12009@byui.edu>
2017-05-17 11:29:14 -0600

    Attempting to fix server Postgress sign in issue

--- web/Project1/db.php
+++ web/Project1/db.php
@@ -3,11 +3,15 @@
 session_start();
 try
 {
-      $user = 'postgres';
-      if ($_SERVER['HTTP_HOST'] == "https://pure-hamlet-55952.herokuapp.com") {
+   if ($_SERVER['HTTP_HOST'] == "https://pure-hamlet-55952.herokuapp.com") {
          $password = '';
-      } else $password = '7510';
-      $db = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=cs313', $user, $password);
+         $user = 'postgres';
+         $db = new PDO('pgsql:host=https://pure-hamlet-55952.herokuapp.com;port=5432;dbname=pure-hamlet-55952::DATABASE', $user, $password);
+      } else {
+         $password = '7510';
+         $user = 'postgres';
+         $db = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=cs313', $user, $password);
+      }
    }
    catch (PDOException $ex)
    {
